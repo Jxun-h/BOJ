b5cdd8a1ec715bb2213379f312a02374d565ccbd
Jxun_h <137363886+Jxun-h@users.noreply.github.com>
2023-08-17 09:41:15 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 7.50 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -0,0 +1,72 @@
+# 블럭 내리기
+def down_block(visited, board):
+    for crashed in visited:
+        a, b = crashed
+        board[a][b] = 0
+
+    m, n = len(board), len(board[0])
+
+    for i in range(m - 1, -1, -1):
+        for j in range(n):
+            if board[i][j] == 0:
+                chaneged = False
+                for k in range(i - 1, -1, -1):
+                    if board[k][j] != 0:
+                        board[i][j], board[k][j] = board[k][j], board[i][j]
+                        chaneged = True
+
+                    if chaneged:
+                        break
+
+
+# 주변 블럭 탐색
+def find_2x2(x, y, board):
+    if board[x][y] == 0:
+        return
+
+    crash_blocks = [[False] * 2 for _ in range(2)]
+
+    for i in range(2):
+        for j in [0, 1]:
+            if board[x][y] == board[i + x][y + j]:
+                crash_blocks[i][j] = (i + x, y + j)
+
+    for i in crash_blocks:
+        if False in i:
+            return
+
+    res = set()
+    for i in crash_blocks:
+        res |= set(i)
+
+    return res
+
+
+def solution(m, n, board):
+    for i in range(m):
+        board[i] = list(board[i])
+
+    visited = set()
+
+    answer = 0
+
+    ag = False
+
+    while 1:
+        # 1 for문으로 블럭 하나씩 다 돌기
+        for a in range(m - 1):
+            for b in range(n - 1):
+                f = find_2x2(a, b, board)
+                if f is not None:
+                    visited |= f
+                    ag = True
+
+        if ag:
+            # 2 블럭 내리기
+            down_block(visited, board)
+            ag = False
+            answer += len(visited)
+            visited = set()
+
+        else:
+            return answer
