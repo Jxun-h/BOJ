782cc84a89fbfa405914482ee77567ebe4871a5d
Jxun_h <137363886+Jxun-h@users.noreply.github.com>
2023-08-17 11:12:21 +0900

    [Gold V] Title: 벽장문의 이동, Time: 3140 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/2666. 벽장문의 이동/벽장문의 이동.py
@@ -0,0 +1,36 @@
+from sys import stdin
+
+n = int(stdin.readline())
+a, b = map(int, stdin.readline().split())
+m = int(stdin.readline())
+ans = 2147000000
+flow = [-1]
+door = [0 if i == a or i == b else 1 for i in range(n+1)]
+for _ in range(m):
+    flow.append(int(stdin.readline()))
+
+
+def go(x, cnt, a):
+    global ans
+    
+    if x == m+1:
+        ans = min(ans, cnt)
+        return
+    
+    cur = flow[x]
+    
+    for i in range(cur, 0, -1):
+        if a[i] == 0:
+            tmp = a[:]
+            tmp[i], tmp[cur] = tmp[cur], tmp[i]
+            go(x+1, cnt+abs(cur-i), tmp)
+    
+    for i in range(cur, n+1):
+        if a[i] == 0:
+            tmp = a[:]
+            tmp[i], tmp[cur] = tmp[cur], tmp[i]
+            go(x+1, cnt+abs(cur-i), tmp)
+
+
+go(1, 0, door)
+print(ans)
