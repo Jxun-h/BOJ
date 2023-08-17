509991de7fa99be4eefec21e999996b701345077
Jxun_h <137363886+Jxun-h@users.noreply.github.com>
2023-08-17 09:38:25 +0900

    [level 2] Title: 오픈채팅방, Time: 62.96 ms, Memory: 53.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
@@ -0,0 +1,23 @@
+def solution(record):
+    result = []
+    answer = []
+    status = ["님이 들어왔습니다.", "님이 나갔습니다."]
+    id_code = {}
+
+    for r in record:
+        l = r.split()
+
+        if l[0] == 'Enter':
+            id_code[l[1]] = l[2]
+            result.append([0, l[1]])
+
+        elif l[0] == 'Leave':
+            result.append([1, l[1]])
+
+        else:
+            id_code[l[1]] = l[2]
+
+    for s, uid in result:
+        answer.append(id_code[uid] + status[s])
+        
+    return answer
