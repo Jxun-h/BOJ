4bdf8832db5ea91bc2ec2dd806715a32bc46c9d3
Jxun_h <137363886+Jxun-h@users.noreply.github.com>
2023-08-18 10:22:05 +0900

    [Silver III] Title: 2×n 타일링, Time: 84 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,14 @@
+from sys import stdin
+
+n = int(stdin.readline().strip())
+dp = [0 for _ in range(1001)]
+
+dp[1], dp[2] = 1, 2
+
+
+for i in range(3, n + 1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n] % 10007)
+
+
