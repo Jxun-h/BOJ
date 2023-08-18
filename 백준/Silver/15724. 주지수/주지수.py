98568d93034c404c548561918657833b3e6edadb
Jxun_h <137363886+Jxun-h@users.noreply.github.com>
2023-08-18 10:57:04 +0900

    [Silver I] Title: 주지수, Time: 356 ms, Memory: 144336 KB -BaekjoonHub

---
+++ 백준/Silver/15724. 주지수/주지수.py
@@ -0,0 +1,16 @@
+from sys import stdin
+
+n, m = map(int, stdin.readline().split())
+dp = [[0] * (m + 1) for _ in range(n + 1)]
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, stdin.readline().split())))
+
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        dp[i][j] = graph[i-1][j-1] + dp[i-1][j] + dp[i][j - 1] - dp[i - 1][j - 1]
+
+
+for _ in range(int(stdin.readline())):
+    x, y, a, b = map(int, stdin.readline().split())
+    print(dp[a][b] - dp[x - 1][b] - dp[a][y - 1] + dp[x - 1][y - 1])
