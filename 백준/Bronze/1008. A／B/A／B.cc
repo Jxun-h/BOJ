2d09d96ac54baaccfe6021ace6275c4d2539bebf
Jxun_h <137363886+Jxun-h@users.noreply.github.com>
2024-01-03 11:57:40 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double n, m;
+    cin >> n >> m;
+
+    cout << fixed;
+    cout.precision(9);
+    
+    cout << (n / m) << endl;
+
+    return 0;
+}
