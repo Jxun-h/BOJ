4f3bd9583ea1ad8efdd48d30924984afd585feba
Jxun_h <137363886+Jxun-h@users.noreply.github.com>
2024-01-03 12:00:06 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long n, m, k;
+    cin >> n >> m >> k;
+
+    cout << fixed;
+    cout.precision(13);
+    
+    cout << (n + m + k) << endl;
+
+    return 0;
+}
