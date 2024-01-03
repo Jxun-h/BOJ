3a4f850733b3f6f1aa551601880b8cf4e51ddd81
Jxun_h <137363886+Jxun-h@users.noreply.github.com>
2024-01-03 14:33:21 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long t, x, y;
+    
+    cin >> t;
+    
+    for (int i = 0; i < t; i ++) {
+        cin >> x >> y;
+        cout << x + y << "\n";
+    }
+    
+    return 0;
+}
