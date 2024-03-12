d66fd3980acba8466e701efe1368601a23975862
Jxun_h <137363886+Jxun-h@users.noreply.github.com>
2024-03-12 17:17:44 +0900

    [Bronze V] Title: 검증수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.py
+++ 백준/Bronze/2475. 검증수/검증수.py
@@ -1,7 +1,4 @@
-from sys import stdin
-arr = list(map(int, stdin.readline().split()))
-ans = 0
-for i in arr:
-    ans += i ** 2
+import sys
 
-print(ans % 10)
+a,b,c,d,e = map(int,sys.stdin.readline().split())
+print((a ** 2 + b ** 2 + c ** 2 + d ** 2 + e ** 2) % 10)
